d5c6f830109e844aef1c02826edd3c5d81e5883e
Gustavo <gustavogarbim850@gmail.com>
2025-06-12 20:59:21 -0300

    Link to /Produtos

--- src/pages/carrinho/carrinho.jsx
+++ src/pages/carrinho/carrinho.jsx
@@ -40,26 +40,32 @@ function Carrinho() {
            <div className={styles.blueLine}></div>
      
            <div className={styles.carousel}>
+            <Link to='/Produtos'>
              <div className={styles.productCard}>
                <h4 className={styles.productTitle}>Quebra Cabeça Dino</h4>
                <img src={Dino} alt="Quebra Cabeça Dino" className={styles.productImage} />
                <p className={styles.productPrice}>R$ 89,90</p>
                <p className={styles.productInstallments}>em até 10x 8,99 <br /> sem juros</p>
              </div>
-     
+            </Link>
+
+            <Link to='/Produtos'>
              <div className={styles.productCard}>
                <h4 className={styles.productTitle}>Livro Matemática</h4>
                <img src={Matematica} alt="Livro Matemática" className={styles.productImage} />
                <p className={styles.productPrice}>R$ 59,90</p>
                <p className={styles.productInstallments}>em até 10x 5,99 <br /> sem juros</p>
              </div>
-     
+            </Link>
+
+            <Link to='/Produtos'>
              <div className={styles.productCard}>
                <h4 className={styles.productTitle}>Camiseta Tucano</h4>
                <img src={Tucano} alt="Camiseta Tucano" className={styles.productImage} />
                <p className={styles.productPrice}>R$ 44,90</p>
                <p className={styles.productInstallments}>em até 10x 4,49 <br /> sem juros</p>
              </div>
+            </Link>
            </div>
          </section>
     </div>
