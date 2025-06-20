527eff2c0dbdc5e4a70a20a4b7e1cf60c8b3c93f
Luana  Portela <165928075+LuaPortela@users.noreply.github.com>
2025-06-20 12:37:21 -0300

    adicionado cadastro fornecedor

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomeIntroducao from '../src/pages/Home-introducao/HomeIntroducao';
 import CadastroEscolhas from '../src/pages/Cadastro-escolha/CadastroEscolha';
-import CadastroProfissionais from '../src/pages/Cadastro-profissional/CadastroProfissional'
-import CadastroMae from '../src/pages/Cadastro-mae/CadastroMae'
-import Carrinho from '../src/pages/carrinho/carrinho'
-import Cuidadores from '../src/pages/cuidadores/HomeSaude'
-import Dados from '../src/pages/dados/dados'
-import Educadores from '../src/pages/Educadores/HomeEducacao'
-import HomePage from '../src/pages/Home-page/HomePage'
-import HomeLoja from '../src/pages/homeloja/homeloja'
-import Instituicoes from '../src/pages/Instituicoes/Instituicoes'
-import Jogos from '../src/pages/Jogos/Jogos'
-import Login from '../src/pages/Login/Login'
-import Planos from '../src/pages/planos/planos'
-import Produtos from '../src/pages/produto/produto'
-import LayoutHeader from '../src/layout/LayoutHeader'
-import LayoutChat from '../src/layout/LayoutChatbot'
-import DashboardPage from '../src/pages/Dashboard/Dashboard'
-import RedeSocial from '../src/pages/Rede-Social/SocialAppLayout'
+import CadastroProfissionais from '../src/pages/Cadastro-profissional/CadastroProfissional';
+import CadastroMae from '../src/pages/Cadastro-mae/CadastroMae';
+import CadastroFornecedores from '../src/pages/Cadastro-fornecedores/CadastroFornecedores';
+import Carrinho from '../src/pages/carrinho/carrinho';
+import Cuidadores from '../src/pages/cuidadores/HomeSaude';
+import Dados from '../src/pages/dados/dados';
+import Educadores from '../src/pages/Educadores/HomeEducacao';
+import HomePage from '../src/pages/Home-page/HomePage';
+import HomeLoja from '../src/pages/homeloja/homeloja';
+import Instituicoes from '../src/pages/Instituicoes/Instituicoes';
+import Jogos from '../src/pages/Jogos/Jogos';
+import Login from '../src/pages/Login/Login';
+import Planos from '../src/pages/planos/planos';
+import Produtos from '../src/pages/produto/produto';
+import LayoutHeader from '../src/layout/LayoutHeader';
+import LayoutChat from '../src/layout/LayoutChatbot';
+import DashboardPage from '../src/pages/Dashboard/Dashboard';
+import RedeSocial from '../src/pages/Rede-Social/SocialAppLayout';
 import LayoutHeaderPublica from './layout/LayoutHeaderPublica';
 
 function App() {
   return (
-
     <>
       <BrowserRouter>
         <Routes>
@@ -33,7 +33,8 @@ function App() {
             <Route path="/CadastroEscolhas" element={<CadastroEscolhas />} />
             <Route path="/CadastroProfissionais" element={<CadastroProfissionais />} />
             <Route path="/CadastroMae" element={<CadastroMae />} />
-            <Route path='/Login' element={<Login/>} />
+            <Route path="/CadastroFornecedores" element={<CadastroFornecedores />} />
+            <Route path="/Login" element={<Login />} />
           </Route>
 
           <Route element={<LayoutHeader />}>
@@ -46,10 +47,8 @@ function App() {
             </Route>
           </Route>
 
-
-          <Route path="/RedeSocial" element={<RedeSocial/>}/>
+          <Route path="/RedeSocial" element={<RedeSocial />} />
           <Route path="/DashboardPage" element={<DashboardPage />} />
-
           <Route path="/Carrinho" element={<Carrinho />} />
           <Route path="/Dados" element={<Dados />} />
           <Route path="/Educadores" element={<Educadores />} />
@@ -61,4 +60,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
